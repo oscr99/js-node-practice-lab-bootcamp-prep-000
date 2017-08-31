72bdd610a25e07427bec30129d7e02287ae1eaab
Orianna Cerato <oricerato@gmail.com>
2017-08-31 22:16:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Emma"
-var height = "68"
+var height = "74"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) + 1)
